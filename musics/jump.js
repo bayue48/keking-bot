fb4f455366c9d8087cfd9020421471553c0dd138
Moch. Bayu Erich Dewangga <70324722+bayue48@users.noreply.github.com>
2021-09-19 11:40:58 +0700

    fixing some bug?

--- musics/jump.js
+++ musics/jump.js
@@ -2,10 +2,9 @@ module.exports = {
   name: "jump",
   inVoiceChannel: true,
   async execute(message, args, client) {
-    client.distube
-      .jump(message, parseInt(args[0]))
-      .catch(() =>
-        message.channel.send(`${client.emotes.error} | Invalid song number.`)
-      );
+    client.distube.jump(message, parseInt(args[0]));
+    // .catch(() =>
+    //   message.channel.send(`${client.emotes.error} | Invalid song number.`)
+    // );
   },
 };
--- musics/seek.js
+++ musics/seek.js
@@ -2,10 +2,9 @@ module.exports = {
   name: "seek",
   inVoiceChannel: true,
   async execute(message, args, client) {
-    client.distube
-      .seek(message, Number(args[0]))
-      .catch(() =>
-        message.channel.send(`${client.emotes.error} | Must be number.`)
-      );
+    client.distube.seek(message, Number(args[0]));
+    // .catch((e) =>
+    //   message.channel.send(`${client.emotes.error} | Must be number.`)
+    // );
   },
 };
