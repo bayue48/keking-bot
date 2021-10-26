bcb06a180d93a15a16c2a5deb116b1dca1103460
bayue48 <48yunyun@gmail.com>
2021-10-26 14:29:05 +0700

    add remove song from queue

---
+++ commands/music/remove.js
@@ -0,0 +1,16 @@
+module.exports = {
+  name: "remove",
+  aliases: ["delete", "rm", "del", "d"],
+  description: "Removes a song from the queue.",
+  inVoiceChannel: true,
+  async execute(message, args, client) {
+    const queue = client.distube.getQueue(message);
+    if (!queue) return message.channel.send("There is no queue.");
+    if (!args[0])
+      return message.channel.send("Please specify a song to remove.");
+    const song = queue.songs[args];
+    if (!song) return message.channel.send("Could not find that song.");
+    queue.songs.splice(queue.songs.indexOf(song), 1);
+    message.channel.send(`Removed ${song.title} from the queue.`);
+  },
+};
