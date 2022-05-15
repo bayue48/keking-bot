362dfad45c75fd000dd0c6054a84e071d5f4eee6
Bayu Erich <rich.bayue@gmail.com>
2022-05-15 20:52:56 +0700

    heroku deploy

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,7 +4,11 @@ module.exports = {
   execute(client) {
     client.user.setActivity({
       type: 'PLAYING',
-      name: `-help for list of commands | ${client.guilds.cache.size} servers | ${client.users.cache.size} users`
+      name: `type '${client.config.prefix}help' for list of commands
+      | onlines: ${client.guilds.cache.size}
+      | servers: ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)}
+      | version: ${client.version}
+      ${client.guilds.cache.size} servers | ${client.users.cache.size} users`,
     });
     console.log(`Ready! Logged in as ${client.user.tag}`);
   }
