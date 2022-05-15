93cd2aa8b26554aadf5f69ab464ed1dbb387f820
Bayu Erich <rich.bayue@gmail.com>
2022-05-15 21:07:02 +0700

    change events message

--- src/events/message.js
+++ src/events/message.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: 'messageCreate',
   execute(message) {
-    console.log(`${message.author.tag} in #${message.channel.name} sent: ${message.content}`);
+    console.log(`${message.author.tag} in server ${message.guild.name} in channel ${message.channel.name} said: ${message.content}`);
   }
 };
