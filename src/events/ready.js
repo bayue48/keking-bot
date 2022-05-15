16fa548c2e370f19ff388fcc0282918f00a82596
Bayu Erich <rich.bayue@gmail.com>
2022-05-15 21:01:07 +0700

    change activity status

--- src/events/ready.js
+++ src/events/ready.js
@@ -5,10 +5,8 @@ module.exports = {
     client.user.setActivity({
       type: 'PLAYING',
       name: `type '-help' for list of commands
-      | onlines: ${client.guilds.cache.size} servers
-      | servers: ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)}
-      | version: ${client.version}
-      | ${client.users.cache.size} users`,
+      | watching ${client.guilds.cache.size} servers
+      | playing with ${client.users.cache.size} users`
     });
     console.log(`Ready! Logged in as ${client.user.tag}`);
   }
